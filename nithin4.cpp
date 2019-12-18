996a5083972bb6ae357963242346761d937afbb6
sainithinp <58968443+sainithinp@users.noreply.github.com>
2019-12-18 12:21:19 +0530

    Add files via upload

---
+++ nithin4.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n%2==0)
+printf("%d",n*n);
+else
+printf("%d",n*n*n);
+return 0;
+}
---
+++ nithin6.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the three numbers")
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b&&a>c)
+	{
+		printf("\n%d is greather than both %d and %d,a,b,c");
+}
+else if(b>a&&b>c)
+{
+	printf("\n%d is greather than both %d and %d,b,a,c);
+}
+else if(c>a&&c>b)
+{
+	printf("\n%d is greather than both %d and %d,c,a,b);
+}
+	else 
+	
+}
+
